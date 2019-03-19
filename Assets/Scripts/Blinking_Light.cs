27cdb42507a766d299bf4dda36e84d8c11a40f3c
Liliannismo33 <gsdfjv@gmail.com>
2019-03-19 09:54:34 +0100

    Update Partie Droite

---
+++ Assets/Scripts/Blinking_Light.cs
@@ -0,0 +1,33 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blinking_Light : MonoBehaviour {
+
+    public float BlinkingInterval;
+    float BlinkingIntervalActual;
+    bool CheckTorF;
+
+    // Use this for initialization
+    void Start () {
+        BlinkingIntervalActual = BlinkingInterval;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        BlinkingIntervalActual = BlinkingIntervalActual - 1;
+
+        if (BlinkingIntervalActual > 0)
+        {
+            Debug.Log("Ohla");
+        }
+
+        if (BlinkingIntervalActual <= 0 )
+        {
+            Debug.Log("Ohla");
+            CheckTorF = gameObject.activeSelf;
+            gameObject.SetActive(!CheckTorF);
+            BlinkingIntervalActual = BlinkingInterval;
+        }
+    }
+}*/
