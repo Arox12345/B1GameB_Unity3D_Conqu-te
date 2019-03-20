96f5e85aacc6b28f66996bb2cc236a7f15659db6
Schwindowsky_Yann <yann.schwindowsky@gmail.com>
2019-03-20 13:49:42 +0100

    Update Spawn PD + Player et PA

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,15 +8,15 @@ public class GameManager : MonoBehaviour
     public GameObject PA;
     public GameObject Player;
 
-    Vector3 PA1 = new Vector3(87, 9, -80);
-    Vector3 PA2 = new Vector3(275, 9, -80);
-    Vector3 PA3 = new Vector3(18, -1, 78);
-    Vector3 PA4 = new Vector3(112, -1, 46);
+    Vector3 PA1 = new Vector3(266, 14, 387);
+    Vector3 PA2 = new Vector3(8, 14, 78);
+    Vector3 PA3 = new Vector3(141, 10, 80.5f);
+    Vector3 PA4 = new Vector3(65, 10, -52);
 
-    Vector3 PD1 = new Vector3(122, 9, 4);
-    Vector3 PD2 = new Vector3(-58, -1, 26);
-    Vector3 PD3 = new Vector3(72, -1, 26);
-    Vector3 PD4 = new Vector3(166, -1, -25);
+    Vector3 PD1 = new Vector3(284, 21, -84);
+    Vector3 PD2 = new Vector3(272, 19, 85);
+    Vector3 PD3 = new Vector3(192, 14, -28);
+    Vector3 PD4 = new Vector3(132, 19, 230);
 
     // Use this for initialization
     void Start()
