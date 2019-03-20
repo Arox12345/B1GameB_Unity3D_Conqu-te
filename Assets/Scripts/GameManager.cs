36ca6dabcc36113e8b421031318d24d705e76235
Liliannismo33 <gsdfjv@gmail.com>
2019-03-20 17:52:31 +0100

    Update Cinemachine

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject PD;
     public GameObject PA;
     public GameObject Player;
+    public GameObject Dolly;
+
+
+    private Vector3 doll;
+    private Vector3 doll2;
 
     Vector3 PA1 = new Vector3(266, 14, 387);
     Vector3 PA2 = new Vector3(8, 14, 78);
@@ -27,12 +33,24 @@ public class GameManager : MonoBehaviour
 
         Instantiate(PA);
         RandomPA();
+
+
+
+
+        Dolly.transform.GetComponent<CinemachineSmoothPath>().m_Waypoints[0].position = doll;
+        Dolly.transform.GetComponent<CinemachineSmoothPath>().m_Waypoints[2].position = doll2;
+
+
+
+        Dolly.transform.GetComponent<CinemachineSmoothPath>().m_Waypoints[1].position = doll - doll2;
+
+        Dolly.transform.GetComponent<CinemachineSmoothPath>().m_Waypoints[1].position.y = 90;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+       
     }
 
     void RandomPD()
@@ -43,18 +61,22 @@ public class GameManager : MonoBehaviour
             case 1:
                 PD.transform.position = PD1;
                 Player.transform.position = PD1;
+                doll2 = PD1;
                 break;
             case 2:
                 PD.transform.position = PD2;
                 Player.transform.position = PD2;
+                doll2 = PD2;
                 break;
             case 3:
                 PD.transform.position = PD3;
                 Player.transform.position = PD3;
+                doll2 = PD3;
                 break;
             case 4:
                 PD.transform.position = PD4;
                 Player.transform.position = PD4;
+                doll2 = PD4;
                 break;
         }
     }
@@ -66,16 +88,22 @@ public class GameManager : MonoBehaviour
         {
             case 1:
                 PA.transform.position = PA1;
+                doll = PA1;
                 break;
             case 2:
                 PA.transform.position = PA2;
+                doll = PA2;
                 break;
             case 3:
                 PA.transform.position = PA3;
+                doll = PA3;
                 break;
             case 4:
                 PA.transform.position = PA4;
+                doll = PA4;
                 break;
         }
     }
+
+  
 }
