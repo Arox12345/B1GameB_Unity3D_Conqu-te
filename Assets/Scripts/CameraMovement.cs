c7a9005de3aad72cb2ff5314c35c9297d9127f42
Schwindowsky_Yann <yann.schwindowsky@gmail.com>
2019-03-21 11:24:14 +0100

    Update Caméra

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+
+    GameObject playerPrefabs;
+	// Use this for initialization
+	void Start ()
+    {
+        playerPrefabs = GameObject.Find("Player(Clone)");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = playerPrefabs.transform.position;
+	}
+}
